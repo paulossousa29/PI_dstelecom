d1c4c2bfd73d7ca7fdf6a39e2806afc207785be4
Rui Vieira <vieira.rui212000@gmail.com>
2022-11-02 19:29:09 +0000

    Login
    Falta por logo e o butão direito

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import {StyleSheet, View} from 'react-native';
+import Login from './pages/Login';
 
 import Navigator from './routes/homeStack';
 
 const App = () => {
-  return <Navigator />;
+  return <Login />;
 };
 
 export default App;
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,9 +1,20 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image, Button, StatusBar} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 
 const Login = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
+    <View style={styles.background}>
+      <Image
+        style={styles.logo}
+        source={require('../assets/logo-black.png')}
+        resizeMode="contain"></Image>
+      <View style={styles.container}>
+        <Text style={styles.text}>Utilizador</Text>
+        <TextInput style={styles.loginInput}></TextInput>
+        <Text style={styles.text}>Palavra-passe</Text>
+        <TextInput style={styles.loginInput}></TextInput>
+        <Button title="Iniciar Sessão"></Button>
+      </View>
     </View>
   );
 };
@@ -11,7 +22,37 @@ const Login = ({navigation}) => {
 export default Login;
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
+    backgroundColor: '#262525',
+  },
+  container: {
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: -100,
+    backgroundColor: '#eee',
+  },
+  loginButton: {
+    width: '80%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+  loginInput: {
+    backgroundColor: '#fff',
+    width: '90%',
+    borderRadius: 50,
+    height: 40,
+    margin: 12,
+    paddingLeft: 10,
+  },
+  logo: {
+    width: '100%',
+  },
+  text: {
+    paddingLeft: 20,
+    paddingTop: 10,
   },
 });
