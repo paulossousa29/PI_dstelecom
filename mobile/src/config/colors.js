97b45b1b7bc7ef4bd8b6cbb4e300dcea2b0724e5
Rui Vieira <vieira.rui212000@gmail.com>
2022-11-03 19:24:00 +0000

    Login feito

---
+++ mobile/src/config/colors.js
@@ -0,0 +1,9 @@
+export default {
+  logoGreyDark: '#262525',
+  logoRed: '#4ecdc4',
+  placeholderGrey: '#c3c3c3',
+  white: '#fff',
+  red: '#e31e24',
+  lightGrey: '#eee',
+  black: '#000',
+};
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,25 +1,84 @@
-import {StyleSheet, View, Text, Image, Button, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
+import colors from '../config/colors';
 
 const Login = ({navigation}) => {
+  const [username, setUsername] = useState(null);
+  const [pass, setPass] = useState(null);
+  const [errorMsgUsername, setErrorMsgUsername] = useState(null);
+  const [errorMsgPass, setErrorMsgPass] = useState(null);
 
   const handleLogin = () => {
     // verificar se dados estao estao corretos
+  };
 
-  }
+  const verification = () => {
+    if (pass == null && username != null) {
+      setErrorMsgPass('Palavra-passe obrigatória!');
+    } else if (pass != null && username == null) {
+      setErrorMsgUsername('Utilizador Obrigatório!');
+    } else if (pass == null && username == null) {
+      setErrorMsgPass('Palavra-passe Obrigatória!');
+      setErrorMsgUsername('Utilizador Obrigatório!');
+    }
+  };
+
+  const validationAccount = () => {
+    if (pass != null && username != null) {
+      setUsername(null);
+      setPass(null);
+      setErrorMsgPass(null);
+      setErrorMsgUsername(null);
+
+      return;
+    }
+    verification();
+  };
 
   return (
     <View style={styles.background}>
       <Image
         style={styles.logo}
-        source={require('../assets/logo-black.png')}
-        resizeMode="contain"></Image>
+        source={require('../assets/logo-black.png')}></Image>
       <View style={styles.container}>
         <Text style={styles.text}>Utilizador</Text>
-        <TextInput style={styles.loginInput}></TextInput>
+        {errorMsgUsername && (
+          <Text style={styles.errorMessage}>{errorMsgUsername}</Text>
+        )}
+
+        <TextInput
+          style={styles.loginInput}
+          onChangeText={setUsername}
+          value={username}
+          placeholder="user@mail.pt"
+          placeholderTextColor="#ccccb3"></TextInput>
+
         <Text style={styles.text}>Palavra-passe</Text>
-        <TextInput style={styles.loginInput}></TextInput>
-        <Button title="Iniciar Sessão"></Button>
+        {errorMsgPass && (
+          <Text style={styles.errorMessage}>{errorMsgPass}</Text>
+        )}
+
+        <TextInput
+          style={styles.loginInput}
+          onChangeText={setPass}
+          value={pass}
+          placeholder="xxxxxxxxxxx"
+          placeholderTextColor="#c3c3c3"></TextInput>
+
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => validationAccount()}>
+          <Text style={styles.buttonText}>Iniciar Sessão</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -30,36 +89,62 @@ export default Login;
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: '#262525',
+    backgroundColor: colors.logoGreyDark,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   container: {
+    flex: 1,
     width: '100%',
     height: '100%',
-    bottom: 0,
+    justifyContent: 'flex-start',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
-    marginTop: -100,
-    backgroundColor: '#eee',
+    backgroundColor: colors.lightGrey,
   },
   loginButton: {
-    width: '80%',
-    height: 70,
-    backgroundColor: '#fc5c65',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    backgroundColor: colors.red,
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginVertical: 30,
+    marginHorizontal: 20,
   },
   loginInput: {
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     width: '90%',
     borderRadius: 50,
     height: 40,
-    margin: 12,
+    marginVertical: 10,
+    marginHorizontal: 20,
     paddingLeft: 10,
   },
   logo: {
-    width: '100%',
+    width: '90%',
+    height: '30%',
+    resizeMode: 'contain',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   text: {
-    paddingLeft: 20,
-    paddingTop: 10,
-    color: '#262525',
+    paddingLeft: 30,
+    paddingTop: 15,
+    color: colors.logoGreyDark,
+    fontWeight: 'bold',
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: 'bold',
+  },
+  errorMessage: {
+    fontSize: 12,
+    color: colors.red,
+    fontWeight: 'bold',
+    paddingLeft: 30,
   },
 });
