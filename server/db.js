ad09f6c0ac3c0da5639107efc5b87b81404a8c03
Beatriz Lacerda <beatriz.lacerda@live.com.pt>
2022-11-16 13:14:54 +0000

    config vars

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "beatrizlacerda",
-    password: "",
-    host: "localhost",
+    user: "ckssxotmwjdfso",
+    password: "b933f75f5a73e990fb0ff9da2c977e8837ae1ba68f35b6cd5d2ec7d186ec6169",
+    host: "ec2-34-193-44-192.compute-1.amazonaws.com",
     port: 5432,
-    database: "dstelecom"
+    database: "d8bv1tru9k6sh3"
 });
 
 module.exports = pool;
