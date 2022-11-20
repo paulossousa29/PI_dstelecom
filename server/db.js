a5e6e3233a6c9a014afbfbe73d04513857e0a6bf
Beatriz Lacerda <beatriz.lacerda@live.com.pt>
2022-11-20 18:55:18 +0000

    database pronta

--- server/db.js
+++ server/db.js
@@ -2,12 +2,10 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-    //user: "ckssxotmwjdfso",
-    //password: "b933f75f5a73e990fb0ff9da2c977e8837ae1ba68f35b6cd5d2ec7d186ec6169",
-    //host: "ec2-34-193-44-192.compute-1.amazonaws.com",
-    //port: 5432,
-    //database: "d8bv1tru9k6sh3",
-    connectionString: "postgres://ckssxotmwjdfso:b933f75f5a73e990fb0ff9da2c977e8837ae1ba68f35b6cd5d2ec7d186ec6169@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d8bv1tru9k6sh3"
+    connectionString: "postgres://ckssxotmwjdfso:b933f75f5a73e990fb0ff9da2c977e8837ae1ba68f35b6cd5d2ec7d186ec6169@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d8bv1tru9k6sh3",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
--- web/client/src/components/Pendentes.js
+++ web/client/src/components/Pendentes.js
@@ -1,279 +1,51 @@
-import axios from "axios";
-import React, { useEffect, useMemo, useState } from "react";
-import { useGlobalFilter, useSortBy, useTable } from "react-table";
-import tw from "twin.macro";
-import { GlobalFilter } from "./GlobalFilter";
-import TablePagination from '@mui/material/TablePagination';
-import { MDBBtn } from 'mdb-react-ui-kit';
-import { TableContainer } from "@mui/material";
-import Paper from '@mui/material/Paper';
+import React, { Fragment, useState, useEffect } from "react";
 
 
- const Table = tw.table`
-  table-fixed
-  text-base
-  text-gray-900
-`;
-
-const TableHead = tw.thead`
-  p-2
-`;
-
-const TableRow = tw.tr`
-border
-border-gray-500
-`;
-
-const TableHeader = tw.th`
-border
-border-gray-500
-p-2
-`;
-
-const TableBody = tw.tbody`
-`;
-
-const TableData = tw.td`
-border
-border-gray-500
-p-5
-`;
+const Pendentes = () => {
+  const [todos, setTodos] = useState([]);
 
 
-const Button = tw.button`
-  pl-4
-  pr-4
-  pt-2
-  pb-2
-  text-black
-  rounded-md
-  bg-red-300
-  hover:bg-red-200
-  transition-colors
-`;
+  async function getTodos() {
+    const res = await fetch("http://localhost:3000/todos");
 
-export default function Pend(props) {
+    const todoArray = await res.json();
 
- 
-    const [products, setProducts] = useState([]);
-
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-    const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-    const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-    // Vai buscar os dados à API
-  const fetchProducts = async () => {
-    const response = await axios
-      .get("https://fakestoreapi.com/products")
-      .catch((err) => console.log(err));
-
-    if (response) {
-      const products = response.data;
-
-      console.log("Products: ", products);
-      setProducts(products);
-    }
-  };
-
+    setTodos(todoArray);
+  }
 
   useEffect(() => {
-    fetchProducts();
+    getTodos();
   }, []);
 
-  // Dados:
-  const data = useMemo(
-    () => [
-      {
-        id: 1,
-        equipa: "equipa1",
-        trabalho: "XAZ",
-        descrição: "Mudança PDO",
-        estado: "Pendente"
-      },
-      {
-        id: 2,
-        equipa: "equipa1",
-        trabalho: "XYB",
-        descrição: "Mudança conetor",
-        estado: "Pendente"
-      },
-      {
-        id: 3,
-        equipa: "equipa2",
-        trabalho: "AJK",
-        descrição: "Mudança PDO",
-        estado: "Pendente"
-      },
-    ],
-    []
-  );
-
-  //Colunas:
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Equipa",
-        accessor: "equipa",
-      },
-      {
-        Header: "Intervenção",
-        accessor: "trabalho",
-      },
-      {
-        Header: "Descrição",
-        accessor: "descrição",
-      },
-      {
-        Header: "Estado do Pedido",
-        accessor: "estado",
-      }
-    ],
-    []
-  );
-
-
-  const productsData = useMemo(() => [...products], [products]);
-
-  const productsColumns = useMemo(
-    () =>
-      products[0]
-        ? Object.keys(products[0])
-            .filter((key) => key !== "rating")
-            .map((key) => {
-              if (key === "image")
-                return {
-                  Header: key,
-                  accessor: key,
-                  Cell: ({ value }) => <img style={{ width: 200, height: 100 }} src={value} />,
-                  maxWidth: 10,
-                };
-
-              return { Header: key, accessor: key };
-            })
-        : [],
-    [products]
-  );
-
-  const tableHooks = (hooks) => {
-    hooks.visibleColumns.push((columns) => [
-      ...columns,
-      {
-        id: "Aut",
-        Cell: ({ row }) => (
-          <MDBBtn rippleColor='dark' color='light' onClick={() => alert("Editing: " + row.values.pedido)}>
-            Autorizar
-          </MDBBtn>
-        ),
-      },
-      {
-        id: "Naut",
-        Cell: ({ row }) => (
-          <MDBBtn rippleColor='dark' color='light' onClick={() => alert("Editing: " + row.values.pedido)}>
-            Não Autorizar
-          </MDBBtn>
-        ),
-      },
-    ]);
-  };
-
-  
-
-  const tableInstance = useTable(
-    {
-      columns,
-      data,
-    },
-    useGlobalFilter,
-    tableHooks,
-    useSortBy
-  );
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    preGlobalFilteredRows,
-    setGlobalFilter,
-    state,
-  } = tableInstance;
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const isEven = (idx) => idx % 2 === 0;
+  console.log(todos);
 
   return (
-    <>
-      <div className="table-responsive" style={{marginLeft:10}}>
-        <GlobalFilter
-          preGlobalFilteredRows={preGlobalFilteredRows}
-          setGlobalFilter={setGlobalFilter}
-          globalFilter={state.globalFilter}
-        />
-      </div>
-      <div className="right-panel" style={{paddingTop: 10, paddingRight: 30, marginLeft:20}}>
-      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="sticky table" {...getTableProps()}>
-            <TableHead>
-              {headerGroups.map((headerGroup) => (
-                <TableRow {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <TableHeader key={column.id} align={column.align} style={{ minWidth: column.minWidth }}
-                      {...column.getHeaderProps(column.getSortByToggleProps())}
-                    >
-                      {column.render("Header")}
-                      {column.isSorted ? (column.isSortedDesc ? " ▼" : " ▲") : ""}
-                      
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              ))}
-            </TableHead>
-            <TableBody {...getTableBodyProps()}>
-              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, idx) => {
-                prepareRow(row);
+    <Fragment>
+      {" "}
+      <table class="table mt-5">
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
 
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1}
-                    {...row.getRowProps()}
-                  >
-                    {row.cells.map((cell, idx) => (
-                      <TableData {...cell.getCellProps()}>
-                        {cell.render("Cell")}
-                      </TableData>
-                    ))}
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-            <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-          </Table>
-        </TableContainer>
-        </Paper>
-      </div>
-    </>
+          {todos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
   );
+};
 
-
-
-}
+export default Pendentes;
