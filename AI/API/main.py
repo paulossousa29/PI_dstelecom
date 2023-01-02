014529fd321292ee589cfc327158dc22f2b76b59
João Pereira <joaoprp111@gmail.com>
2023-01-02 15:36:43 +0000

    API da AI atualizada

--- AI/API/main.py
+++ AI/API/main.py
@@ -1,4 +1,6 @@
 from src.server.instance import server
 from src.controllers.models import *
 
-server.run()
+if __name__ == '__main__':
+    app = server.app
+    app.run(debug=True)
--- AI/API/test_api/post_image.py
+++ AI/API/test_api/post_image.py
@@ -1,5 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/detect'
+url = 'https://database-dst.herokuapp.com/detect'
+#url = 'http://127.0.0.1:5000/detect'
 files = {'file': open('../static/images/test.jpg','rb')}
 requests.post(url,files=files)
