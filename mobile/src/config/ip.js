3b62e2d88ebf2ccd64ccc4fda04b5e28c0f8836a
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2023-01-10 19:15:21 +0000

    commit

--- mobile/src/config/ip.js
+++ mobile/src/config/ip.js
@@ -1,5 +1,4 @@
 export default {
-  backend_ip: 'http://192.168.1.241:3001/',
-  //api_ip: 'http://192.168.1.137:5000/',
-  api_ip: 'https://database-dst.herokuapp.com/',
+  backend_ip: 'http://database-dst.herokuapp.com/',
+  api_ip: 'https://app-dst.herokuapp.com/',
 };
