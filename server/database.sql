63b54cd10302d5933b7c33bdecf70d99d0e469fe
Beatriz Lacerda <beatriz.lacerda@live.com.pt>
2023-01-16 23:53:51 +0000

    new table

--- server/database.sql
+++ server/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE skill(id INT GENERATED ALWAYS AS IDENTITY,id_equipa VARCHAR(255) NO
 
 CREATE TABLE intervencoes(id VARCHAR(255) NOT NULL,acesso VARCHAR(255) ,elemento VARCHAR(255) ,conector INT ,id_equipa VARCHAR(255) ,CONSTRAINT fk_equipa FOREIGN KEY(id_equipa) REFERENCES equipas(id), PRIMARY KEY(id));
 
-CREATE TABLE relatorios(id INT GENERATED ALWAYS AS IDENTITY,id_intervencao VARCHAR(255) NOT NULL,passo_1 INT NOT NULL,passo_3 INT NOT NULL,passo_5 INT NOT NULL,passo_7 INT NOT NULL,passo_9 INT NOT NULL,passo_11 INT NOT NULL,passo_13 INT NOT NULL,observacoes VARCHAR(1000),data_inicio timestamp,data_fim timestamp, CONSTRAINT fk_equipa FOREIGN KEY(id_intervencao) REFERENCES intervencoes(id),PRIMARY KEY(id));
+CREATE TABLE relatorios(id INT GENERATED ALWAYS AS IDENTITY,id_intervencao VARCHAR(255) NOT NULL,passo_1 INT NOT NULL,passo_3 INT NOT NULL,passo_5 INT NOT NULL,passo_7 INT NOT NULL,passo_9 INT NOT NULL,passo_11 INT NOT NULL,passo_12  INT NOT NULL, passo_13 INT NOT NULL,observacoes VARCHAR(1000),data_inicio timestamp,data_fim timestamp, CONSTRAINT fk_equipa FOREIGN KEY(id_intervencao) REFERENCES intervencoes(id),PRIMARY KEY(id));
 
 CREATE TABLE pedidos(id INT GENERATED ALWAYS AS IDENTITY, id_intervencao VARCHAR(255) NOT NULL, estado INT NOT NULL, descricao VARCHAR(1000), CONSTRAINT fk_equipa FOREIGN KEY(id_intervencao) REFERENCES intervencoes(id),PRIMARY KEY(id));
 
