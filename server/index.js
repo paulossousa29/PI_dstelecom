77b6fd222785f9e109b598ff4893b20d60837731
Beatriz Lacerda <beatriz.lacerda@live.com.pt>
2023-01-18 23:45:35 +0000

    novos pedidos

--- server/index.js
+++ server/index.js
@@ -235,32 +235,6 @@ app.post("/element", async (req, res) => {
 	}
 });
 
-app.get("/pedidos/:start/:end", async (req, res) => {
-	console.log("request pedidos");
-	const start = req.params.start;
-	const end = req.params.end;
-	obj = [];
-	try {
-		pool.connect();
-
-		const allTodos = await pool.query(
-			"SELECT * FROM pedidos WHERE estado = 0 LIMIT " + end + "OFFSET " + start + ";"
-		);
-		console.log(allTodos.rows);
-		allTodos.rows.forEach((c) => {
-			obj.push({
-				id_intervencao: c.id_intervencao,
-				estado: "Suspenso",
-				descricao: c.descricao,
-				id: c.id,
-			});
-		});
-
-		res.json(obj);
-	} catch (err) {
-		console.error(err.message);
-	}
-});
 
 app.post("/new_reference", async (req, res) => {
 	console.log("request new reference");
@@ -463,16 +437,46 @@ app.post("/report", async (req, res) => {
 	}
 });
 
-app.get("/pedidosaceites/:id", async (req, res) => {
+
+app.get("/pedidos/:start/:end", async (req, res) => {
+	console.log("request pedidos");
+	const start = req.params.start;
+	const end = req.params.end;
+	obj = [];
+	try {
+		pool.connect();
+
+		const allTodos = await pool.query(
+			"SELECT * FROM pedidos WHERE estado = 0 LIMIT " + end + "OFFSET " + start + ";"
+		);
+		console.log(allTodos.rows);
+		allTodos.rows.forEach((c) => {
+			obj.push({
+				id_intervencao: c.id_intervencao,
+				estado: "Suspenso",
+				descricao: c.descricao,
+				id: c.id,
+			});
+		});
+
+		res.json(obj);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+app.get("/pedidosaceites/:id/:start/:end", async (req, res) => {
 	console.log("request pedidos aceites");
 	const id = req.params.id;
+	const start = req.params.start;
+	const end = req.params.end;
 	try {
 		pool.connect();
 		const allTodos = await pool.query(
 			"UPDATE pedidos SET estado = 1 WHERE id = " + id + ";"
 		);
 		const final = await pool.query(
-			"SELECT * FROM pedidos WHERE estado = 0;"
+			"SELECT * FROM pedidos WHERE estado = 0 LIMIT " + end + "OFFSET " + start + ";"
 		);
 		res.json(final.rows);
 		console.log(final);
@@ -481,16 +485,18 @@ app.get("/pedidosaceites/:id", async (req, res) => {
 	}
 });
 
-app.get("/pedidosrecusados/:id", async (req, res) => {
+app.get("/pedidosrecusados/:id/:start/:end", async (req, res) => {
 	console.log("request pedidos recusados");
 	const id = req.params.id;
+	const start = req.params.start;
+	const end = req.params.end;
 	try {
 		pool.connect();
 		const allTodos = await pool.query(
 			"UPDATE pedidos SET estado = 2 WHERE id = " + id + ";"
 		);
 		const final = await pool.query(
-			"SELECT * FROM pedidos WHERE estado = 0;"
+			"SELECT * FROM pedidos WHERE estado = 0 LIMIT " + end + "OFFSET " + start + ";"
 		);
 		res.json(final.rows);
 		console.log(final);
